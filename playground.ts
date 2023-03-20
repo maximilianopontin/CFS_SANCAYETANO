74cab0d8ea9ff50329e87933bd10c015b3aae46e
maximilianopontin <maxipontin92@gmail.com>
2023-03-20 19:59:47 -0300

    ejercicio piloto de carrera

--- playground.ts
+++ playground.ts
@@ -1,13 +1,17 @@
-/* calcule el area de un rectangulo pero para cualquier base y altura
-* el usuario debe ingresar la base y la altura por teclado
-*el area debe guardarse en una variable
-*el resultado debe ser mostrado por pantalla
+/* en una prueba un piloto tiene que completar 4 vueltas
+*se necesita un programa quew permita ingresar por teclado el tiempo de cada vuelta
+*el programa debe retornar el tiempo total y promedio de vuelta
 */
-import * as rls from "readline-sync";
-let base:number=rls.questionInt("ingrese la base:");
-let altura:number=rls.questionInt("ingrese la altura:");
-let area:number= base*altura;
-console.log ("el area es:" , area);
+import * as rls from "readline-sync"
+
+const lap1:number=rls.questionFloat("ingrese el tiempo de la primera vuelta");
+const lap2:number=rls.questionFloat("ingrese el tiempo de la segunda vuelta");
+const lap3:number=rls.questionFloat("ingrese el tiempo de la tercer vuelta");
+const lap4:number=rls.questionFloat("ingrese el tiempo de la cuarta vuelta");
+const totalTime:number=lap1+lap2+lap3+lap4;
+const media:number=totalTime/4;
+console.log("el tiempo total es de", totalTime)
+console.log("el tiempo promedio de vuelta es de",media)
 
 
 

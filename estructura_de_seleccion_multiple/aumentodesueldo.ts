88da7d952c6b9f09ea29af2e5c3139ac602471b5
maximilianopontin <maxipontin92@gmail.com>
2023-03-28 19:23:17 -0300

    ejercicios con condicionales

---
+++ estructura_de_seleccion_multiple/aumentodesueldo.ts
@@ -0,0 +1,13 @@
+/* Una empresa desea premiar a sus empleados con un
+aumento de sueldo. Este aumento se ajusta a la siguiente
+tabla:
+56
+Sueldo Actual Sueldo con Aumento
+0 - 15.000 $ 20%
+15.001 - 20.000 $ 10%
+20.001 - 25.000 $ 5%
+Más de 25.000 $ No hay aumento
+• Desarrolle un algoritmo dado el salario actual de un
+empleado determine el aumento de sueldo a aplicar y
+se lo muestre
+*/
---
+++ estructura_de_seleccion_multiple/descuentooctubre.ts
@@ -0,0 +1,8 @@
+/* Una tienda al cumplir años en Octubre ofrece
+un descuento del 15% a sus clientes en todas
+sus compras
+• Desarrolle un algoritmo que dada una compra:
+precio unitario, cantidad y el mes, indicados
+por el usuario, determine si el cliente tiene
+descuento o no
+*/

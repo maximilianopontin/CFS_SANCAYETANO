0417fb1ca345f40429c3163b57e8a3b4d05edb31
maximilianopontin <maxipontin92@gmail.com>
2023-04-06 20:54:34 -0300

    ejercicio de calificaciones para entrega

--- estructuradecontrolrepetitiva/calificaciones.ts
+++ estructuradecontrolrepetitiva/calificaciones.ts
@@ -13,3 +13,24 @@ alumno hasta que el nombre sea una cadena vacía
 Las notas deben estar entre 0 y 10 (si no lo están, no imprimirá las
 notas, mostrará un mensaje de error y continuará con otro alumno)
 */
+
+import * as rls from "readline-sync";
+
+let nombreAlumno: string= rls.question ("ingrese nombre de alumno:");
+let porcentajePractica:number= 0.1;
+let porcentajeProblema:number= 0.5;
+let porcentajeTeoria:number= 0.4;
+let notaFinal:number = 0;
+
+while (nombreAlumno !=""){
+    let notaPractica:number = rls.questionInt ("ingrese nota de la parte practica:");
+    let notaProblemas:number= rls.questionInt ("ingrese nota de la parte problemas:");
+    let notaTeorica:number=rls.questionInt ("ingrese nota de la parte teorica:");
+
+if ((notaPractica >10 || notaPractica <0)|| (notaProblemas >10 || notaProblemas <0)||(notaTeorica >10 || notaTeorica <0)) {
+    console.log ("error al ingresar una de sus notas, ingrese un numero valido");
+} else {
+    notaFinal = (notaPractica * porcentajePractica ) + (notaProblemas * porcentajeProblema)+ (notaTeorica * porcentajeTeoria);
+    console.log ("Su nota final es:",notaFinal );
+}   nombreAlumno = rls.question ("ingrese nombre de alumno:");
+}

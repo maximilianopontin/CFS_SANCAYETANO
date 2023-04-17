70278f216e75561101499c0b34d47010423c25c6
maximilianopontin <maxipontin92@gmail.com>
2023-04-17 19:45:13 -0300

    ejercicios de practica de funciones con arreglos

--- modularizacion/arrays.ts/ejerciciosPractica.ts
+++ modularizacion/arrays.ts/ejerciciosPractica.ts
@@ -88,7 +88,7 @@ console.log(nombres, numeros);
 Construya un algoritmo que tenga un arreglo de números y se los muestre al usuario
 El arreglo debe ser llamado num 
 El arreglo num debe contener los siguientes datos: 20, 14, 8, 0, 5, 19 y 24.
-Mostrar los valores resultantes del arreglo
+Mostrar los valores resultantes del arreglo:
 */
 
 
